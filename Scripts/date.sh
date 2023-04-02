80bfa7281d499a93df46d68a09d75c8bbefa35e5
DeividWilly <deividwillydev@gmail.com>
2023-04-02 17:19:00 -0300

    added scripts files for dwmblocks

---
+++ Scripts/date.sh
@@ -0,0 +1,2 @@
+data=$(date +"%d/%b/%Y - %H:%M:%S")
+echo "$data"
---
+++ Scripts/rofi.sh
@@ -0,0 +1,1 @@
+rofi -combi-modi window,drun,ssh,combi -theme DarkBlue -font "monospace 11" -show combi -icon-theme "Papirus" -show-icons
