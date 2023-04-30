a41b58561774f13e741b225d8d73b00511d7748e
DeividWilly <deividwillydev@gmail.com>
2023-04-30 17:46:34 -0300

    test nvim dotfile

---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,25 @@
+-- Setting local variables to vim.g/o to g or o
+
+local g = vim.g
+local o = vim.o
+
+-- Editor Basic's --
+
+o.clipboard = 'unnamedplus'
+o.number = true
+o.relativenumber = false
+o.cursorline = false
+o.hidden = true
+o.inccommand = 'split'
+o.mouse = 'a'
+o.smartcase = true
+o.smartindent = true
+o.fileencoding = 'utf-8'
+o.ignorecase = true
+o.splitbelow = true
+o.splitright = true
+o.tabstop = 4
+
+
+-- Theme
+
