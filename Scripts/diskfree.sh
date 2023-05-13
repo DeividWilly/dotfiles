4b2ac05c702597675c9633e847a8aa0f3dc9717b
DeividWilly <deividwillydev@gmail.com>
2023-05-13 15:04:39 -0300

    diskfree script improved

--- Scripts/diskfree.sh
+++ Scripts/diskfree.sh
@@ -1,2 +1,3 @@
-disk=$(df -h --total | grep total | awk '{print $3"/"$4}')
-echo "$disk"
+disk_used=$(df -h | grep /home | awk '{print$3}')
+disk_avail=$(df -h | grep /home | awk '{print$4}')
+echo "Used: $disk_used / Avail: $disk_avail"
